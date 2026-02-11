bbcb211f9d25858793bce162d23026c025e7f195
Ramanand Yadav <virat1322420@gmail.com>
2026-02-11 05:45:35 +0000

    pushing the opps folder

---
+++ 37.py
@@ -0,0 +1,20 @@
+name = "ramsingh"
+# string  slicing (accesssing)
+# print(name[2])
+# print(name[1:6])
+# print(name[:6])
+# print(name[4:])
+# print(name[:])
+
+# for x in name:
+#   print(x)
+
+newmname = name[0:3]
+
+firstchar = name[0:]
+a = name[:]
+print(name)
+print(newmname)
+print(firstchar)
+print(a)
+print(f"{name} is name")
---
+++ 38.py
@@ -0,0 +1,16 @@
+# import math
+# a = math.pi
+# print(a)
+
+
+with open("1.py","r") as a:
+  readMessage = a.read()
+  print(readMessage)
+print(type(readMessage))
+# arr = readMessage.split(" ")
+# print(arr)
+# for x in range(len(arr)):
+#   if arr[x] =="dog":
+#     # print("message is abusive")
+#     arr[x]="##"
+# print(arr)
